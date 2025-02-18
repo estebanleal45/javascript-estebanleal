f710dab5f18b01c0a2c965f705a44d3118055ae2
Esteban <estebanleal547@gmail.com>
2025-02-18 11:33:27 -0500

    Ejercicios

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let nom = "Juan Esteban Leal";
+let nombre = "Juan Esteban Leal";
 let edad ="18";
-let ciu ="Garzón-Huila";
-console.log(" Mi nombre es " + nom + ", tengo " + edad + " años y vivo en " + ciu);
+let ciudad ="Garzón-Huila";
+console.log(" Mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad);
 
 /*Crea una variable llamada precio y asignale un valor decimal. 
 Luego crea otra variable llamada cantidad y asignale un valor entero. 
@@ -13,3 +13,18 @@ let cantidad =5;
 console.log(typeof precio); // imprime en la consola
 document.write (typeof cantidad); // imprime en el navegador
 alert (typeof cantidad); // imprime en una ventana emergente
+
+
+// ACTIVIDAD EJERCICIOS EN JAVASCRIPT
+// 1
+let descuento = 0.25;
+let valor = 5000;
+let total = valor * descuento;
+console.log(total);
+
+
+// 2
+let capital = 10000000;
+let interes = 0.2;
+let ganacia = capital + interes;
+console.log(ganacia);
