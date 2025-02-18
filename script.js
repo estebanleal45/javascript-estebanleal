a5b3354601cc7b3e381a850d1225cbbd6b3c5c16
Esteban <estebanleal547@gmail.com>
2025-02-18 09:54:07 -0500

    Ejercicio 1 y 2

---
+++ script.js
@@ -0,0 +1,15 @@
+let nom = "Juan Esteban Leal";
+let edad ="18";
+let ciu ="Garzón-Huila";
+console.log(" Mi nombre es " + nom + ", tengo " + edad + " años y vivo en " + ciu);
+
+/*Crea una variable llamada precio y asignale un valor decimal. 
+Luego crea otra variable llamada cantidad y asignale un valor entero. 
+Muestra el tipode dato de cada variable en la consola.*/
+
+
+let precio = 3.14;
+let cantidad =5;
+console.log(typeof precio); // imprime en la consola
+document.write (typeof cantidad); // imprime en el navegador
+alert (typeof cantidad); // imprime en una ventana emergente
